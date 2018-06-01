4d70ae75cff23645b64d012fbaeb035d070928a2
oskii2311 <os.janczak@gmail.com>
2018-06-01 18:40:41 +0200

    remove registry worker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
@@ -14,4 +13,3 @@ ReactDOM.render(
 	<Provider store={store}>
 		<App />
 	</Provider>, document.getElementById('root'));
-registerServiceWorker();

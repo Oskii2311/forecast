2b37726e7a1ca140353bfd904fc7f32222e9fe35
oskii2311 <os.janczak@gmail.com>
2018-06-06 17:00:48 +0200

    bootstrap rsp table

--- src/containers/WeatherList/weather_list.jsx
+++ src/containers/WeatherList/weather_list.jsx
@@ -20,23 +20,25 @@ class WeatherList extends Component {
 
   render() {
     return (
-      <table className="table table-hover weather">
-        <thead>
-          <tr>
-            <th>City</th>
-            <th>Temperature (C)</th>
-            <th>Pressure (hPa)</th>
-            <th>Humidity (%)</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            this.props.weather.map(cityData =>
-              <Weather key={cityData.city.id} cityData={cityData} />)
-          }
-          {this.checkIfErrorOrLoading()}
-        </tbody>
-      </table>
+      <div class="table-responsive">
+        <table className="table table-hover weather">
+          <thead>
+            <tr>
+              <th>City</th>
+              <th>Temperature (C)</th>
+              <th>Pressure (hPa)</th>
+              <th>Humidity (%)</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.props.weather.map(cityData =>
+                <Weather key={cityData.city.id} cityData={cityData} />)
+            }
+            {this.checkIfErrorOrLoading()}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
